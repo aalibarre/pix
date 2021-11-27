9e06a0ba85643ba4b585960953f87a5e2c565cad
Xihai Luo <xihai.luo01@gmail.com>
2021-11-27 22:43:16 +0000

    add seeds for users and restaurants

---
+++ db/seeds/users.sql
@@ -0,0 +1,5 @@
+-- insert user seeds
+INSERT INTO users (phone_number, username, email, password)
+VALUES ('6473307003', 'Xihai Luo', 'xihai.luo01@gmail.com', 'h3llosdcard');
+INSERT INTO users (phone_number, username, email, password)
+VALUES ('4163218092', 'Amy Garfield', 'avg123@aol.com', 'sunburst');
