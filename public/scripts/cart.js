c924822a9606f97ecf9762ccf7c6179bb9f0bb12
Amir <amir.a.23@hotmail.com>
2021-11-28 23:40:42 +0000

    Add comments for cart.js

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,4 +1,27 @@
+// Client facing scripts here
+const cart = {
+  name: [0, 0],
+};
 
+//add to cart
+//remove from cart
+//clear cart
+//change cart
+addtocart () {
+  let name =
+}
+function getCart()
+function clearCart() {
+  sessionStorage.removeItem("mycart");
+}
+myStorage = window.sessionStorage;
 $(document).ready(() => {
-
+  //console.log("hi");
+  $('.add-cart').click((event) => {
+    cart.name = [1,2];
+    sessionStorage.setItem('key', cart);
+    console.log(sessionStorage);
+    //alert("ADDED TO CART");
+    event.preventDefault();
+  });
 });
