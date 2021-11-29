bb2b9d2acd34ef5cea99696e7b7a61d8ac198eee
Xihai Luo <xihai.luo01@gmail.com>
2021-11-29 04:18:01 +0000

    add code to fetch item name from clicking qty btn

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
   $('.button-minus').on('click', function(e) {
     //get the qty counter elm
     const qtyCounter = $(this).parent().find('span');
+    const name = $(this).closest('article').find('h2');
     let qty = Number(qtyCounter.text());
     //decrement counter only if counter is not 0
-    if (qty > 0) {
-      console.log(qty);
+    if (qty !== 0) {
+      console.log(name.text());
       qty -= 1;
+      console.log(qty);
       qtyCounter.text(qty);
     }
   });
@@ -16,11 +18,13 @@ $(document).ready(function() {
   $('.button-plus').on('click', function(e) {
     //get the qty counter elm
     const qtyCounter = $(this).parent().find('span');
+    const name = $(this).closest('article').find('h2');
     let qty = Number(qtyCounter.text());
     //increment counter until 10. 10 is max qty to order
-    if (qty < 10) {
-      console.log(qty);
+    if (qty !== 10) {
+      console.log(name.text());
       qty += 1;
+      console.log(qty);
       qtyCounter.text(qty);
     }
   });
