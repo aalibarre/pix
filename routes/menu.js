3d0a564b310e0964c1af59703c242b3463564fed
Amir <amir.a.23@hotmail.com>
2021-11-30 01:03:56 +0000

    Add post menu route logic, update checkout.ejs and server.js

--- routes/menu.js
+++ routes/menu.js
@@ -26,34 +26,26 @@ module.exports = (db) => {
   router.post("/", (req, res) => {
     //the data sent by ajax in emu.ejs is stored in req.body
     /* everything below is for debugging */
-    //console.log(req.body);
     if(!req.session.cart) {
       req.session.cart = {};
     }
     const data = req.body;
-    //console.log('NAME: ', data);
     //if data key already exists
     let name = Object.keys(data)[0];
     //name = name.pop();
     let quantity = parseInt(data[name]);
-    //console.log(typeof name,typeof quantity);
-    //console.log(name, quantity);
     console.log('CART Before >>>>>>>>>>>>', req.session.cart);
     console.log('Name >>>>>>>>>>>>', name);
-    if(req.session.cart[name]) {
-      req.session.cart[name] += quantity;
-    } else {
-      req.session.cart[name] = quantity;
-    }
-    console.log('Temp Data ---------> ', data);
-    console.log('CART >>>>>>>>>>>>', req.session.cart);
+    console.log('Quantity type', typeof quantity);
+
+    req.session.cart[name] = quantity;
     res.status(200);
     res.send();
-    //if data is zero
-    //console.log('Data in cookie session: ', req.session.cart);
-    //console.log(Object.keys(data));
+    console.log('Temp Data ---------> ', data);
+    console.log('CART >>>>>>>>>>>>', req.session.cart);
 
-    //res.send({ data });
+    //if data is zero
+
     //when adding to db
     //add to cart, server side
     //ajax request for button press
@@ -62,23 +54,18 @@ module.exports = (db) => {
 
   // /menu/checkout routes
   router.get("/checkout", (req, res) => {
-    // const user_id = 1;
-    // req.session.user_id = user_id;
-    // if (!req.session.userId) {
-    //   res.error("💩");
-    //   console.log('Error user_id is not correct', user_id);
-    //   return;
-    // }
+
     const listOfOrders = req.session.cart;
+    console.log(listOfOrders);
     //console.log('Order List ===>', orderList);
     //console.log(typeof orderList);
     if(listOfOrders) { //!Add check for user id
       //loop through all cart
       //query for price of food item
       //add to cart obj
-      for (let order of listOfOrders) {
+      for (let order in listOfOrders) {
 
-        db.query(`SELECT price FROM menu WHERE name = $1`, [order.name])
+        db.query(`SELECT price FROM meals WHERE name = $1`, [order])
         .then((result) => {
           console.log(result);
           return;
@@ -89,13 +76,17 @@ module.exports = (db) => {
         });
 
       }
-      totalPrice = 10;
+      let quantity = Object.values(req.session.cart);
+      quantity = parseInt(quantity.pop());
+      console.log('Quantity >>>>> ', quantity);
+      console.log(typeof quantity);
+      totalPrice = 10 * quantity;
       //let totalPrice = cartObj.quantity * cart; Put in loop
       // for (let item of cart) {
       //   totalPrice += (cart[item].price * cart[item].qty);
       // }
 
-      let templateVars = {orders:listOfOrders, totalPrice};
+      let templateVars = {listOfOrders, totalPrice};
       res.render("checkout", templateVars);
       //template
       //res.render
@@ -105,19 +96,19 @@ module.exports = (db) => {
       //redirect to main (or show relevent error)
     }
 
-    db.query(`SELECT total_price, total_quantity FROM orders WHERE user_id = 2 AND orders.id = 1;`)
-    .then(data => {
-      let orders = data.rows;
-      console.log(orders);
-      res.render('checkout', {orders});
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-        console.log('######Error######');
-        console.log(err.message);
-    });
+    // db.query(`SELECT total_price, total_quantity FROM orders WHERE user_id = 2 AND orders.id = 1;`)
+    // .then(data => {
+    //   let orders = data.rows;
+    //   console.log(orders);
+    //   res.render('checkout', {orders});
+    // })
+    // .catch(err => {
+    //   res
+    //     .status(500)
+    //     .json({ error: err.message });
+    //     console.log('######Error######');
+    //     console.log(err.message);
+    // });
   });
 
   router.post("/checkout", (req, res) => {
@@ -125,6 +116,13 @@ module.exports = (db) => {
     //  when user confirms checkout add items to orders table and redirect to menu page
     //  db.query(`INSERT INTO orders (resturant_id, user_id, name, total_quantity, total_price) VALUES (1, 2, 'Grandma's Creamery', 10, 60) RETURNING*`)
 
+
+
+    //sms to owner of new order
+    //once we get a reply from owner
+    //save to order history
+    //send sms to customer with order confirmation
+    //redirect order history or menu
     return res.redirect(`/menu`);
    });
    return router;
