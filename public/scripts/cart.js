646d834ed82e20a47d1552d5e64f6b21695bc10b
Amir <amir.a.23@hotmail.com>
2021-11-30 23:36:01 +0000

    Add a template for items in the cart

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,71 +1,31 @@
-
-//Initializations
-//prefixes ("name, quantity, total, etc..")
-/* this.storage = sessionStorage;
-const cart = {
-  name: "Strawberry",
-  quantity: 1,
-  price: 10
-};
-
-
- createCart = () => {
-  if(this.storage.getItem( this.cartName ) == null) {
-      let cart = {};
-      cart.items = [];
-      this.storage.setItem(this.cartName, this.toJSONString( cart ));
-      this.storage.setItem(this.quantity, "0");
-      this.storage.setItem(this.price, "0");
-  //} */
-//};
-//save food item
-//add to cart
-//loop through add to cart buttons class,
-//add event listener on click
-//on click store values into session
-
-//addToCart = () => {
-  //cap on items in storage
-
-//}
-//remove from cart
-
-//clear cart
-//change cart
+//cart container
+//render cart data
+//render new cart data, append
+//update cart
+//checkout
+//delete cart item
+//add quantity
 
 
 
-///ON MENU PAGE
-/*
-add to cart, change quantity, update price?
-save to sessionstorage
-*/
+const createEmptyCart = () => {
 
+}
 
-//myStorage = window.sessionStorage;
-/* $(document).ready(() => {
-  //console.log("hi");
-
-  $('.add-cart-btn').click((event) => {
-    sessionStorage.setItem('item', cart);
-    console.log(sessionStorage.getItem('item'));
-    //alert("ADDED TO CART");
-    event.preventDefault();
-  });
-}); */
+const createCartElement = cartData => {
+  let div = `<div class="cart-item">
+              <span class="item-name">Cookie</span>
+              <p class="price">$6.00</p>
+              <button class="remove-item-btn">X</button>
+            </div>`;
+  return div;
+}
 
 
 
-///ON CHECKOUT PAGE
-/*
-on load get data in sessionstorage
-store in variables
-set values in cart table to those variables
-
-Can, remove, change quantity, update price, cancel order, make purchase
 
 
-*/
+
 
 
 //Helper Functions
